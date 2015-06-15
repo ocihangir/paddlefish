77c5b49c527fe25018fb96abfbcf0a836c4365c5
Mustafa Aldemir <mustafa@Mustafa-ING-MacBook-Pro.local>
2015-06-15 19:02:47 +0300

    Dashboard initial commit

---
+++ android/PaddleFish/app/src/main/java/net/aldemir/paddlefish/ui/MonitorActivity.java
@@ -0,0 +1,17 @@
+package net.aldemir.paddlefish.ui;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import net.aldemir.paddlefish.R;
+
+public class MonitorActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitor);
+    }
+}
